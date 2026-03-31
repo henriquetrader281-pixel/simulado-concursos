0568751c5c4de6a5a0b389f2c8feeb73b5566e7b
henriquetrader281-pixel <henriquetrader281@gmail.com>
2026-03-31 17:23:18 -0300

    Create bb_01.js

---
+++ data/bb_01.js
@@ -0,0 +1,20 @@
+window.bb_01 = [
+  {
+    disciplina: "portugues",
+    pergunta: "Assinale a frase correta:",
+    opcoes: ["Houveram erros", "Houve erros", "Houveram problema", "Nenhuma"],
+    resposta: 1
+  },
+  {
+    disciplina: "matematica",
+    pergunta: "10% de 500:",
+    opcoes: ["10", "50", "100", "500"],
+    resposta: 1
+  },
+  {
+    disciplina: "bancarios",
+    pergunta: "PIX é:",
+    opcoes: ["Moeda", "Pagamento instantâneo", "Cartão", "Banco"],
+    resposta: 1
+  }
+];
